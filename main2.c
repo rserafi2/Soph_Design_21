5562c3bde82f6d5f5f5bbe512325166a672a10cc
Ryan <rserafi2@binghamton.edu>
2021-05-27 20:14:11 -0400

    Almost done commenting

--- main2.c
+++ main2.c
@@ -5,7 +5,6 @@ Description: This code was to make the Polulu 3pi into a programmable robot that
 took the paramaters of movement such as Forward, reverse, clockwise and
 counter-clockwise, speeds of 3 different settings, and time incremented by 0.1.
 */
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,17 +37,16 @@ void printTopLCD(char message[8]) {
   LCD_move_cursor_to_col_row(0, 0);
   LCD_print_String(message);
 }
-
 // Prints message on bottom row of the LCD
 void printBotLCD(char message[8]) {
   LCD_move_cursor_to_col_row(0, 1);
   LCD_print_String(message);
 }
-
+// Prints message to the right of the last message printed
 void printAppendLCD(char message[8]) {
   LCD_print_String(message);
 }
-// Configures left motor to the direction specified
+// Configures motors to the direction specified
 void configMotors(int direction) {
   switch (direction) {
   // Forward
@@ -87,7 +85,6 @@ void configMotors(int direction) {
     PORTB |= (1 << R_MOTOR_2);
   }
 }
-
 /*
 Makes the robot move according to the instructions specified.
 int movement = the direction specified
@@ -97,7 +94,7 @@ int movement = the direction specified
   3: Reverse
   Other: No movement (Pause)
 int speed = speed of robot from 0-100 (not moving - maximum speed of motor)
-int time = how long the movement should last in tenths of a second
+int time = how long the movement should last based off TIME_CONSTANT
 */
 void runInstruction(int movement, int time, int speed) {
   int move_time = 0;
@@ -111,8 +108,7 @@ void runInstruction(int movement, int time, int speed) {
       if (pwm_counter >= PWM_TOP) {
         pwm_counter = 0;
       }
-      // Speed of each motor is based on pwm
-      // wheel_offset affects the motor that was slightly fast
+      // Speed of motors are based on pwm
       if (pwm_counter < speed) {
         configMotors(movement);
       } else {
@@ -122,7 +118,7 @@ void runInstruction(int movement, int time, int speed) {
     }
   }
 }
-
+//More specified move functions. All based off runInstruction
 void moveForward(int time, int speed){
   runInstruction(0,time,speed);
 }
@@ -142,7 +138,7 @@ void moveTurnLeft(int time, int speed){
 void moveStop(int time){
   runInstruction(4,time,2);
 }
-
+//Makes the buzzer beep for a specified amount of time.
 void setBuzzer(int time){
   int buzz_time = 0;
   int pwm_counter = 0;
@@ -152,7 +148,7 @@ void setBuzzer(int time){
     if (pwm_counter >= PWM_TOP) {
       pwm_counter = 0;
     }
-    // Speed of each motor is based on pwm
+    // Volume of buzzer based off BUZZER_VOL (0-100 low to high)
     if (pwm_counter < BUZZER_VOL) {
       PORTB |= (1 << BUZZER);
     } else {
@@ -188,6 +184,7 @@ int main() {
   char bunkers_claimed_string[8];
   char blanks[8] = "        ";
   int i;
+  
   // Configure Motor pins and ports
   DDRD |= (1 << L_MOTOR_1) | (1 << L_MOTOR_2) | (1 << R_MOTOR_1);
   DDRB |= (1 << R_MOTOR_2);
@@ -200,10 +197,10 @@ int main() {
   // Configure right push-button
   DDRB &= ~(1 << R_BUTTON);
   PORTB |= (1 << R_BUTTON);
-
-  DDRC &= ~(1<<SENSOR_0) & ~(1<<SENSOR_1) & ~(1<<SENSOR_2) & ~(1<<SENSOR_3) & ~(1<<SENSOR_4); //configure sensors
+  // Configure sensors
+  DDRC &= ~(1<<SENSOR_0) & ~(1<<SENSOR_1) & ~(1<<SENSOR_2) & ~(1<<SENSOR_3) & ~(1<<SENSOR_4);
   PORTC |= (1<<SENSOR_0) | (1<<SENSOR_1) | (1<<SENSOR_2) | (1<<SENSOR_3) | (1<<SENSOR_4);
-
+  // Configure buzzer
   DDRB |= (1 << BUZZER);
 
   initialize_LCD_driver();
@@ -238,7 +235,7 @@ int main() {
     } else {
       right_button_pressed = 0;
     }
-
+    //Interface stage of program
     switch (interface_stage) {
     case 0:
     	printTopLCD("SPEED?  ");
@@ -277,14 +274,25 @@ int main() {
     	break;
     }
   }
-
+  /*Robot runs program until the specified amount of bunkers are claimed. Below is the strategy:
+    1. Move forward until a sensor reads black underneath it (a value of 1)
+    2. Once a sensor reads 1, have the robot sweep over the source of the black tape
+      a. if when sweeping, 4 or more sensors read 1 at the same time, the source must be a wall
+        Then it will turn around at a random angle and continue moving forward again
+      b. if when sweeping, less than 4 sensors read 1 at the same time, the source must be a bunker.
+        Then the buzzer will go off and it will back up from the bunker, turn around, and wait for a moment. It will then continue moving forward
+        The bunker count will go up by one after finding a bunker. If it was the last bunker, then it will back up from the bunker, spin twice and beep
+    While the robot is going, the sensor values will be displayed along with how many bunkers have already been found
+  */
   while(bunkers_claimed < bunkers_max){
+    //update sensors
     sensor_data[0] = (PINC & (1<<SENSOR_0));
     sensor_data[1] = (PINC & (1<<SENSOR_1));
     sensor_data[2] = (PINC & (1<<SENSOR_2));
     sensor_data[3] = (PINC & (1<<SENSOR_3));
     sensor_data[4] = (PINC & (1<<SENSOR_4));
 
+    //Displays # bunkers found out of the number of bunkers set
     printTopLCD("#:");
   	itoa(bunkers_claimed, bunkers_claimed_string, 10);
   	printAppendLCD(bunkers_claimed_string);
@@ -292,6 +300,9 @@ int main() {
   	printAppendLCD(bunkers_max_string);
     printAppendLCD(blanks);
   	LCD_move_cursor_to_col_row(0,1);
+
+    //Checks raw data from sensors, if greater than 0, then the state value is set to 1
+    //Loop also adds what the sensor reading is to the display
     for(i = 0; i<5; i++){
       if(sensor_data[i] > 0){
           sensor_state[i] = 1;
@@ -304,7 +315,7 @@ int main() {
     }
     printAppendLCD(blanks);
 
-    if(bunker_found){
+    if(bunker_found){//If a bunker was found, turn left until the robot is facing the bunker and then do the movements specified
       if((!sensor_state[0] && !sensor_state[1] && sensor_state[2] && !sensor_state[3] && !sensor_state[4])||
          (!sensor_state[0] && sensor_state[1] && sensor_state[2] && sensor_state[3] && !sensor_state[4])){
         moveBackward(70,SPEED_SLOW);
@@ -316,7 +327,7 @@ int main() {
         moveTurnLeft(1,SPEED_SLOW);
       }
     }
-    else if(!on_black){
+    else if(!on_black){//If the robot is not sensing any black tape, then move forward a small amount. When a sensor reads 1, stop moving
       if(sensor_state[0] || sensor_state[1] || sensor_state[2] || sensor_state[3] || sensor_state[4] ){
         moveStop(30);
         on_black = 1;
@@ -325,8 +336,8 @@ int main() {
         moveForward(1,speed);
       }
     }
-    else if(on_black){
-      if(!scanning){
+    else if(on_black){//If the robot has found black tape, start scanning the source
+      if(!scanning){//To prep the robot for scanning, it turns left until only the rightmost sensor reads 1
         if(!sensor_state[0] && !sensor_state[1] && !sensor_state[2] && !sensor_state[3] && sensor_state[4] ){
           moveStop(20);
           scanning = 1;
@@ -335,11 +346,12 @@ int main() {
           moveTurnLeft(1,SPEED_SLOW);
         }
       }
-      else if(scanning){
+      else if(scanning){//The robot turns right a little and checks how many sensors are reading 1 at the same time.
+                        //It keeps turning right until only the leftmost sensor reads 1.
         if(sensor_state[0] && !sensor_state[1] && !sensor_state[2] && !sensor_state[3] && !sensor_state[4] ){
           moveTurnRight(50+(rand()%200),SPEED_SLOW);
           moveStop(20);
-          if(max_sensors_black < 4){
+          if(max_sensors_black < 4){//If there were not 4 or more sensors on at the smae time then a bunker must have been detected
             setBuzzer(10000);
             bunkers_claimed ++;
             bunker_found = 1;
@@ -358,7 +370,7 @@ int main() {
       }
     }
   }
-  while(!victory){
+  while(!victory){//This loop runs after the last bunker is detected and ends when the victory movements are done
     sensor_data[0] = (PINC & (1<<SENSOR_0));
     sensor_data[1] = (PINC & (1<<SENSOR_1));
     sensor_data[2] = (PINC & (1<<SENSOR_2));
@@ -373,9 +385,9 @@ int main() {
       }
     }
     if((!sensor_state[0] && !sensor_state[1] && sensor_state[2] && !sensor_state[3] && !sensor_state[4])||
-       (!sensor_state[0] && sensor_state[1] && sensor_state[2] && sensor_state[3] && !sensor_state[4])){
+       (!sensor_state[0] && sensor_state[1] && sensor_state[2] && sensor_state[3] && !sensor_state[4])){//Robot turns left until facing the bunker directly
       moveBackward(70,SPEED_SLOW);
-      moveTurnRight(230,SPEED_FAST);
+      moveTurnRight(200,SPEED_FAST);
       moveStop(2);
       setBuzzer(10000);
       setBuzzer(10000);
